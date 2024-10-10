343a162228ff8dc84cfd783b88012b77b1d24b62
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2024-10-10 20:32:34 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# ColosseumTrophy
+
+Makes the cut content available.
+
+# EUPL
+                      Copyright (c) 2024 SFGrenade
+                      Licensed under the EUPL-1.2
+https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
---
+++ src/ColosseumTrophy.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using JetBrains.Annotations;
+using Modding;
+using SFCore.Utils;
+using UnityEngine.SceneManagement;
+
+namespace ColosseumTrophy;
+
+[UsedImplicitly]
+internal class ColosseumTrophy : Mod
+{
+    internal static ColosseumTrophy Instance;
+
+    public ColosseumTrophy() : base("Colosseum Gold Trophy")
+    {
+        InitCallbacks();
+    }
+
+    public override string GetVersion() => Util.GetVersion(Assembly.GetExecutingAssembly());
+
+    public override void Initialize()
+    {
+        Log("Initializing");
+        Instance = this;
+
+        Log("Initialized");
+    }
+
+    private void InitCallbacks()
+    {
+        // Hooks
+        UnityEngine.SceneManagement.SceneManager.activeSceneChanged += OnSceneChanged;
+    }
+
+    private void OnSceneChanged(Scene from, Scene to)
+    {
+        if (to.name != "Room_Colosseum_Bronze" && to.name != "Room_Colosseum_Silver" && to.name != "Room_Colosseum_Gold")
+        {
+            return;
+        }
+        to.Find("colosseum gold trophy").SetActive(true);
+    }
+}
